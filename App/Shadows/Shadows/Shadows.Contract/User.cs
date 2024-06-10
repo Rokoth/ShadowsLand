6731988dfc4663167697adb87c4fa86b2a103d87
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2024-06-10 10:48:58 +0500

    [Feature-0.0.1] Добавил модель User

--- App/Shadows/Shadows/Shadows.Contract/User.cs
+++ App/Shadows/Shadows/Shadows.Contract/User.cs
@@ -6,34 +6,21 @@ namespace Shadows.Contract
     public class User : Entity
     {
         [Display(Name = "Имя")]
-        [Required(ErrorMessage = "Поле должно быть установлено")]
-        [Remote("CheckName", "User", ErrorMessage = "Имя уже используется")]
+        [Required(ErrorMessage = "Поле должно быть установлено")]        
         public string Name { get; set; }
 
         [Display(Name = "Описание")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         [Display(Name = "Логин")]
-        [Required(ErrorMessage = "Поле должно быть установлено")]
-        [Remote("CheckLogin", "User", ErrorMessage = "Логин уже используется")]
+        [Required(ErrorMessage = "Поле должно быть установлено")]       
         public string Login { get; set; }
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-        [Display(Name = "Только листовые элементы")]
-        public bool LeafOnly { get; set; }
-        [Display(Name = "ИД формулы")]
-        public Guid FormulaId { get; set; }
-        [Display(Name = "Формула")]
-        public string Formula { get; set; }
-        [Display(Name = "Дата последнего добавления элемента в резервы")]
-        public DateTimeOffset? LastAddedDate { get; set; }
-        [Display(Name = "Величина резерва по умолчанию")]
-        [Required(ErrorMessage = "Поле должно быть установлено")]
-        public decimal DefaultReserveValue { get; set; }
 
-        [Display(Name = "Период добавления элементов в резервы")]
-        [Required(ErrorMessage = "Поле должно быть установлено")]
-        public int AddPeriod { get; set; }
+        [Display(Name = "Email")]
+        public string? Email { get; set; }
+
+        [Display(Name = "Phone")]
+        public string? Phone { get; set; }
 
     }
 }
